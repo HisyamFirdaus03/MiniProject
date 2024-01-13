8de8aba766861daa064bf7cbc2e5ca04b6641966
HisyamFirdaus03 <hisyamfirdaus100@gmail.com>
2024-01-13 22:31:47 +0800

    Commit Starter Files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "src/Counter.hpp"
+#include "src/Menu.hpp"
+#include "src/Patient.hpp"
+#include "src/Queue.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
