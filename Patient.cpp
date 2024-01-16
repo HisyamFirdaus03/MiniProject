3588c0f60bf9f7f634e9b6109e1030e439ed0aaa
HisyamFirdaus03 <hisyamfirdaus100@gmail.com>
2024-01-16 19:08:11 +0800

    Updated Patient

--- Patient.cpp
+++ Patient.cpp
@@ -1,1 +1,33 @@
 #include "src/Patient.hpp"
+
+Patient :: Patient(string _name, int _age) : name(_name), age(_age), waitingNumber(-1), roomNumber(-1) {}
+
+string Patient :: getName() const{
+
+    return name;
+}
+
+int Patient :: getAge() const{
+
+    return age;
+}
+
+int Patient :: getRoomNumber() const{
+
+    return roomNumber;
+}
+
+int Patient :: getWaitingNumber() const{
+
+    return waitingNumber;
+}
+
+void Patient :: assignRoom(int _room){
+
+    roomNumber = _room;
+}
+
+void Patient :: assignWaitingNumber(int _waitingNumber){
+
+    waitingNumber = _waitingNumber;
+}
--- src/Patient.hpp
+++ src/Patient.hpp
@@ -1,6 +1,20 @@
 #pragma once
+#include <string>
+using namespace std;
 
 class Patient
 {
+    private:
+        string name;
+        int age, waitingNumber, roomNumber;
 
+    public:
+        Patient *next;
+        Patient(string _name, int _age);
+        string getName() const;
+        int getAge() const;
+        int getRoomNumber() const;
+        int getWaitingNumber() const;
+        void assignRoom(int _room);
+        void assignWaitingNumber(int _waitingNumber);
 };
